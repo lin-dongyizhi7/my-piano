c7ed5297850a64b8076ff9ac02c4f2ff0db9ad77
Lindongyizhi <56852070+lin-dongyizhi7@users.noreply.github.com>
2023-02-25 16:56:59 +0800

    change

--- mypiano/myJS/getKey.js
+++ mypiano/myJS/getKey.js
@@ -7,36 +7,49 @@ var charKey = [
 
 var block0 = [0, 422, 842, 1263];
 var block1 = [0, 70, 140, 176, 246, 316, 386];
-
-var mp3s = [];
-for (var i = 1; i <= 8; i++) {
-    var mp3url = "mypiano/audios/" + i + ".mp3";
-    console.log(mp3url);
-    //var mp3 = new Audio(mp3url);
-    //var myAuto = document.getElementById('myaudio');
-    //myAuto.src = mp3url;//MP3路径
-    // myAuto.load();
-    //mp3.load();
-    //mp3s.push(mp3);
-}
+var flag = 1;
 
 // var cl = document.querySelector(".keyBoard");
 // console.log(cl);
+
+function myFocus() {
+    flag = 0;
+}
 
 function keyboard(event) {
     let keycode = event.keyCode;
     var num = charKey.indexOf(keycode);
     //alert(num);
+    if (num < 0 || flag == 0) return;
+    playing(num);
+}
+
+function draw(x, y) {
+    //var y = Math.random() * 400 + 100;
+    //console.log(y);
+    var colors = [Math.round(Math.random() * 255), Math.round(Math.random() * 255), Math.round(Math.random() * 255)];
+    for (var r = 0; r < 20; r += 1) {
+        c.fillStyle = `rgba(${colors[0]},${colors[1]},${colors[2]},${(30 - r) / 30})`;
+        //console.log(c.fillStyle);
+        c.beginPath();
+        c.arc(x, y, r, 0, 2 * Math.PI);
+        c.stroke();
+        c.closePath();
+    }
+}
+
+function playing(num) {
     var zu = Math.floor(num / 7);
     var q = num % 7;
-
     var l1 = block0[zu];
     var l2 = block1[q];
     var l = l1 + l2;
     //console.log(zu);
     //console.log(q);
     //console.log(l);
-    playing(l);
+    c.fillStyle = "rgba(51,201,22,1)";
+    c.fillRect(l, 600, 35, 202);
+
     var n = num + 1;
     var mp3url = "./audios/" + n + ".mp3";
     var a = document.createElement("audio");
@@ -49,17 +62,25 @@ function keyboard(event) {
     s.setAttributeNode(type);
     a.append(s);
     var autoplay = document.createAttribute("autoplay");
-    autoplay.value="autoplay";
+    autoplay.value = "autoplay";
     a.setAttributeNode(autoplay);
-    console.log(a);
+    //console.log(a);
 
     // var main = document.getElementById("main");
     // main.appendChild(a);
+    draw(l + 18, 500 - n * 15);
 }
 
-function playing(left) {
-    c.fillStyle = "rgba(51,201,22,1)";
-    c.fillRect(left, 600, 35, 202);
+function mouseClick(event) {
+    let x = event.screenX;
+    let y = event.screenY;
+    // console.log("click");
+    // console.log(x);
+    // console.log(y);
+    if (y > 150) {
+        flag = 1;
+    }
 }
 
 document.addEventListener("keydown", keyboard);
+document.addEventListener("click", mouseClick);
